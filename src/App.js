06bc0f38b2326d8dfabae254eeb551bd8d239bdd
WJ Lee <wjlee2122@gmail.com>
2021-07-10 10:45:38 +0900

    setting up react router and yarn add other dependencies

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import { lazy, Suspense } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+import * as ROUTES from './constants/routes';
 
 const Dashboard = lazy(() => import('./pages/dashboard'));
 const Login = lazy(() => import('./pages/login'));
@@ -8,9 +11,17 @@ const NotFound = lazy(() => import('./pages/not-found'));
 
 function App() {
   return (
-    <Suspense fallback={<p>Loading...</p>}>
-      <p>This is where the content will be</p>
-    </Suspense>
+    <Router>
+      <Suspense fallback={<p>Loading...</p>}>
+        <Switch>
+          <Route path={ROUTES.LOGIN} component={Login} />
+          <Route path={ROUTES.SIGN_UP} component={SignUp} />
+          <Route path={ROUTES.PROFILE} component={Profile} />
+          <Route exact path={ROUTES.DASHBOARD} component={Dashboard} />
+          <Route path={ROUTES.NOT_FOUND} component={NotFound} />
+        </Switch>
+      </Suspense>
+    </Router>
   );
 }
 
