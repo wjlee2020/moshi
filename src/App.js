0e148e48cd9f80aea82ae9156efc3b3712e3ecd6
WJ Lee <wjlee2122@gmail.com>
2021-07-10 10:05:19 +0900

    setting up lazy

--- src/App.js
+++ src/App.js
@@ -1,3 +1,11 @@
+import { lazy, Suspense } from 'react';
+
+const Dashboard = lazy(() => import('./pages/dashboard'));
+const Login = lazy(() => import('./pages/login'));
+const SignUp = lazy(() => import('./pages/signup'));
+const Profile = lazy(() => import('./pages/profile'));
+const NotFound = lazy(() => import('./pages/not-found'));
+
 function App() {
   return (
     <div>
