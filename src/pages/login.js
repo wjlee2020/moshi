2bfb9c1a627b891029dccc7246a77e6d9afe3988
WJ Lee <wjlee2122@gmail.com>
2021-07-10 12:00:18 +0900

    Update login.js

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,25 +1,44 @@
+import * as ROUTES from '../constants/routes';
+import { Link } from 'react-router-dom';
+
 export default function Login() {
 
     return (
         <div className="container mx-auto flex max-w-screen-md items-center h-screen">
             <div className="flex w-3/5">
-                <img className="w-4/6 h-3/4 rounded-lg"
+                <img className="w-4/5 h-3/4 rounded-lg"
                     src=" https://images.unsplash.com/photo-1511707171634-5f897ff02aa9?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80"
                     alt='phone' />
             </div>
             <div className="flex flex-col w-2/5">
-                <h1 className="flex justify-center w-full">
-                    Moshi?
-                </h1>
-                <div>
-                    <form>
-                        <input />
-                        <input />
-                        <button>Log In</button>
+                <div className="flex flex-col items-center bg-white p-4 border mb-4">
+                    <h1 className="flex justify-center w-full">
+                        Moshi?
+                    </h1>
+                    <form method="post">
+                        <input
+                            placeholder="Email Address"
+                            type="text"
+                            aria-label="Enter your email address"
+                            className="text-sm w-full mr-3 py-5 px-4 h-2 border rounded mb-2" />
+                        <input
+                            placeholder="Password"
+                            type="password"
+                            aria-label="Enter your password"
+                            className="text-sm w-full mr-3 py-5 px-4 h-2 border rounded mb-2" />
+                        <button type="submit"
+                            className={`bg-blue-500 text-white w-full rounded h-8 font-bold`}>
+                            Log In
+                        </button>
                     </form>
                 </div>
-                <div>
-                    <p>Already have an Account? Sign Up</p>
+                <div className="flex justify-center items-center flex-col w-full bg-white p-4 border">
+                    <p className="text-sm">
+                        Don't have an account?{' '}
+                        <Link to={ROUTES.SIGN_UP} className="font-bold">
+                            Sign up
+                        </Link>
+                    </p>
                 </div>
             </div>
         </div>
