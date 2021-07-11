e3984de7aeffee620470dc56c33e4bd1963335b2
WJ Lee <wjlee2122@gmail.com>
2021-07-11 18:21:11 +0900

    setting up sign up page

--- src/customHooks/useDocumentTitle.js
+++ src/customHooks/useDocumentTitle.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 
 const useDocumentTitle = title => {
     const [documentTitle, setDocumentTitle] = useState(title)
