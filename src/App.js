69d50f99a58b587fc1e5e0aff5981a3847fd19b0
WJ Lee <wjlee2122@gmail.com>
2021-07-17 18:21:41 +0900

    setting up auth listener for user state

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import { lazy, Suspense } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
+import useAuthListener from './customHooks/useAuthListener';
 import * as ROUTES from './constants/routes';
+import UserContext from './context/user';
 
 const Dashboard = lazy(() => import('./pages/dashboard'));
 const Login = lazy(() => import('./pages/login'));
@@ -10,18 +12,22 @@ const Profile = lazy(() => import('./pages/profile'));
 const NotFound = lazy(() => import('./pages/not-found'));
 
 function App() {
+  const { user } = useAuthListener();
+
   return (
-    <Router>
-      <Suspense fallback={<p>Loading...</p>}>
-        <Switch>
-          <Route path={ROUTES.LOGIN} component={Login} />
-          <Route path={ROUTES.SIGN_UP} component={SignUp} />
-          <Route path={ROUTES.PROFILE} component={Profile} />
-          <Route exact path={ROUTES.DASHBOARD} component={Dashboard} />
-          <Route component={NotFound} />
-        </Switch>
-      </Suspense>
-    </Router>
+    <UserContext.Provider value={{ user }}>
+      <Router>
+        <Suspense fallback={<p>Loading...</p>}>
+          <Switch>
+            <Route path={ROUTES.LOGIN} component={Login} />
+            <Route path={ROUTES.SIGN_UP} component={SignUp} />
+            <Route path={ROUTES.PROFILE} component={Profile} />
+            <Route exact path={ROUTES.DASHBOARD} component={Dashboard} />
+            <Route component={NotFound} />
+          </Switch>
+        </Suspense>
+      </Router>
+    </UserContext.Provider>
   );
 }
 
