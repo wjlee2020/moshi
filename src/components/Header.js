ec974025b6a92c8aa4fb61ef1f7f21bcb2057209
WJ Lee <wjlee2122@gmail.com>
2021-07-17 12:43:37 +0900

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,15 @@
+import { Link } from "react-router-dom"
+import * as ROUTES from '../constants/routes';
+import { useContext } from "react"
+import FirebaseContext from "../context/firebase"
+
 export default function Header() {
+
     return (
-        <p>I'm the Header</p>
+        <div className="bg-gray-100 p-5">
+            <Link to={ROUTES.DASHBOARD}>
+                <button className="text-2xl font-bold rounded p-1 px-2 bg-blue-200 hover:bg-blue-400 hover:text-white">Moshi Moshi</button>
+            </Link>
+        </div>
     )
 }
