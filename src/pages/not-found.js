a9624e06e7847fafed5ca851437a65fc330d8d4f
WJ Lee <wjlee2122@gmail.com>
2021-07-17 12:32:13 +0900

    Update not-found.js

--- src/pages/not-found.js
+++ src/pages/not-found.js
@@ -9,14 +9,14 @@ export default function NotFound() {
     useDocumentTitle('Moshi? - Page Not Found')
     return (
         <div className="bg-gray-500 p-5">
-            <div className="mx-auto max-w-screen-lg text-center text-white">
+            <div className="mx-auto max-w-screen-lg text-center text-white text-2xl">
                 <h1 className="m-5">404 - Page Not Found</h1>
                 <p>Sorry the page you were looking for doesn't exist.</p>
-                <p>Click the button below to return to the dashboard.</p>
+                <p>Click the button below to return to the login screen.</p>
                 <button
                     className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded m-5"
-                    onClick={() => { history.push(ROUTES.DASHBOARD) }}>
-                    Dashboard
+                    onClick={() => { history.push(ROUTES.LOGIN) }}>
+                    Go Back!
                 </button>
             </div>
         </div>
