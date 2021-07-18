484b197607ceb1fc6ee1a5d0663548fa0cb778c3
WJ Lee <wjlee2122@gmail.com>
2021-07-18 10:31:35 +0900

    implementing react loading skeleton for timeline

--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -1,5 +1,11 @@
+import Skeleton from 'react-loading-skeleton';
+
 export default function Timeline() {
+    const photos = null;
+
     return (
-        <p>I'm the Timeline</p>
+        <div className="container col-span-2">
+            {photos ? photos.map((photo, i) => (<p key={i}>I'm the photos</p>)) : <Skeleton count={5} width={640} height={500} className="mb-5" />}
+        </div>
     )
 }
--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -11,8 +11,8 @@ export default function Dashboard() {
         <div className="bg-gray-200">
             <Header />
             <div className="grid grid-cols-3 gap-4 justify-between mx-auto max-w-screen-lg">
-                <Sidebar />
                 <Timeline />
+                <Sidebar />
 
             </div>
         </div>
