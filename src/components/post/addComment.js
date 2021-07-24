81102920f87c7fde3d5a633577a88f49b5c78010
Lee <wjlee2122@gmail.com>
2021-07-24 12:02:25 +0900

    setting up the addComment component for adding comments
    handleSubmitComment function to firebase update

--- src/components/post/addComment.js
+++ src/components/post/addComment.js
@@ -1,7 +1,34 @@
 import { useState, useContext } from 'react';
+import FirebaseContext from '../../context/firebase';
+import UserContext from '../../context/user';
 
-export default function AddComment() {
+export default function AddComment({docId, comments, setComments, commentInput}) {
+    const [comment, setComment] = useState('');
+
+    const { firebase, FieldValue } = useContext(FirebaseContext)
+    const { user: {displayName} } = useContext(UserContext);
+
+    const handleSubmitComment = (event) => {
+        // not async, don't want to have network call prior to state updates. don't have to worry about taking time for the network to connect and then having to update [imo]
+        event.preventDefault();
+
+        //check seed file for structure for users data to see why spreading comments
+        setComments([{ displayName, comment}, ...comments]);
+        setComment('');
+
+        return firebase
+            .firestore()
+            .collection('photos')
+            .doc(docId)
+            .update({
+                // arrayUnion === merge into existing array
+                comments: FieldValue.arrayUnion({ displayName, comment })
+            })
+    }
+
     return (
-        null
+        <div className="border-t border-gray">
+            <input className="p-4 pt-1 pb-4 text-sm text-gray-500" placeholder="add a comment..." />
+        </div>
     )
 }
--- src/components/post/comments.js
+++ src/components/post/comments.js
@@ -5,31 +5,32 @@ import { formatDistance } from 'date-fns';
 
 export default function Comments({docId, comments: allComments, posted, commentInput}) {
     const [ comments, setComments ] = useState(allComments);
-    
-    console.log(comments);
-    const postedComments = comments.map((comment, i) => (
-        <div key={i}>
-            <p>{comment.comment}</p>
-            <p className="font-bold">{comment.displayName}</p>
-        </div>
-    ))
 
     return (
-        <div className="p-4 pt-1 pb-4">
-            {comments.length >= 3 && (
-                <p className="text-sm text-gray-500 mb-1 cursor-pointer">View All {comments.length} comments</p>
-            )}
-            {comments.slice(0, 3).map((comment) => (
-                <p className="mb-1" key={`${comment.comment}-${comment.displayName}`}>
-                    <Link to={`/p/${comment.displayName}`}>
-                        <span className="mr-1 font-bold">{comment.displayName}</span>
-                    </Link>
-                    <span>- {comment.comment}</span>
+        <>
+            <div className="p-4 pt-1 pb-4">
+                {comments.length >= 3 && (
+                    <p className="text-sm text-gray-500 mb-1 cursor-pointer">View All {comments.length} comments</p>
+                )}
+                {comments.slice(0, 3).map((comment) => (
+                    <p className="mb-1" key={`${comment.comment}-${comment.displayName}`}>
+                        <Link to={`/p/${comment.displayName}`}>
+                            <span className="mr-1 font-bold">{comment.displayName}</span>
+                        </Link>
+                        <span>- {comment.comment}</span>
+                    </p>
+                ))}
+                <p className="text-gray uppercase text-xs mt-2">
+                    {formatDistance(posted, new Date())} ago
                 </p>
-            ))}
-            <p className="text-gray uppercase text-xs mt-2">
-                {formatDistance(posted, new Date())} ago
-            </p>
-        </div>
+            </div>
+            <AddComments 
+            // sending in state/setState for comments. parent holds state, AddComments child updates states
+                docId={docId}
+                comments={comments}
+                setComments={setComments}
+                commentInput={commentInput}
+            />
+        </>
     )
 }
