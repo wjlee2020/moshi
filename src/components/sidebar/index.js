b5d44690c6ed6bf3c75983a319f5f10b49d3dfca
Lee <wjlee2122@gmail.com>
2021-07-24 12:50:30 +0900

    setting up sidebar

--- src/components/sidebar/index.js
+++ src/components/sidebar/index.js
@@ -1,10 +1,13 @@
 import useUser from "../../customHooks/useUser"
+import { User } from './user';
 
 export default function Sidebar() {
     // default of empty obj when we don't have an activeUser (a user signed in)
     const { activeUser: { docId, userId, following, username, fullName } = {} } = useUser();
 
     return (
-        <p>I'm the sidebar</p>
+        <div className="p-4">
+            <User username={username} fullName={fullName} />
+        </div>
     )
 }
