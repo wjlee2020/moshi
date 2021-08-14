485f0402f964b561b7c1104e19e0677c767cdbcc
WJ Lee <wjlee2122@gmail.com>
2021-08-14 17:04:23 +0900

    some edits
    trying out new feature + starting out with profile header

--- src/components/profile/header.js
+++ src/components/profile/header.js
@@ -1,9 +1,16 @@
 import { useState, useEffect } from "react";
 
-export default function Header({username, profile, photosCount, followerCount, setFollowerCount}) {
+export default function Header({ username, profile, photosCount, followerCount, setFollowerCount }) {
+    console.log(profile)
     return (
-        <div className="border border-black">
-            <p>hello this is the header for {username}</p>
+        <div className="grid grid-cols-3 gap-4 justify-between mx-auto max-w-screen-lg">
+            <div className="container flex justify-center">
+                <img
+                    className="rounded-full h-40 w-40 flex"
+                    src={`/images/avatars/${username}.jpg`}
+                    alt={`${username} profile`}
+                />
+            </div>
         </div>
     )
 }
--- src/components/profile/photos.js
+++ src/components/profile/photos.js
@@ -1,14 +1,17 @@
+import { useState } from 'react';
 import Skeleton from 'react-loading-skeleton';
 
-export default function Photos({photosCollection}) {
+export default function Photos({ photosCollection }) {
     // console.log(photosCollection)
+    const [hoveredPhoto, setHoveredPhoto] = useState(false)
+    const [imgLikes, setImgLikes] = useState('');
+    console.log(hoveredPhoto);
 
     function enter(photoInfo) {
         console.log(photoInfo)
+        setImgLikes(photoInfo.likes.length)
+        setHoveredPhoto(true)
         // setIsHovered(true);
-    }
-    function leave(photoInfo) {
-        // console.log(photoInfo)
     }
 
     // future task: add onhover with the comments length & add the likes
@@ -17,22 +20,21 @@ export default function Photos({photosCollection}) {
     return (
         <div className="h-16 border-t border-gray mt-12 pt-4">
             <div className="grid grid-cols-3 gap-5 mt-4 mb-12">
-            {!photosCollection ? (
-                <>
-                    {[...new Array(photosCollection && photosCollection.length)].map((_, index) => (
-                        <Skeleton key={index} count={1} width={320} height={400} />
-                    ))}
-                </>
-            ) : photosCollection && photosCollection.length > 0 ? ( photosCollection && 
-                photosCollection.map((photo, index) => (
-                    <div key={index} onMouseEnter={() => enter(photo)} onMouseLeave={() => leave(photo)}>
-                        <img className="rounded-md" src={photo.imageSrc} alt={photo.caption} />
-                        {/* {photo.comments.length} */}
-                    </div>
-                ))
-            ) : null}
+                {!photosCollection ? (
+                    <>
+                        {[...new Array(photosCollection && photosCollection.length)].map((_, index) => (
+                            <Skeleton key={index} count={1} width={320} height={400} />
+                        ))}
+                    </>
+                ) : photosCollection && photosCollection.length > 0 ? (photosCollection &&
+                    photosCollection.map((photo, index) => (
+                        <div key={index} >
+                            <img className="rounded-md" src={photo.imageSrc} alt={photo.caption} onMouseEnter={() => enter(photo)} onMouseLeave={() => setHoveredPhoto(false)} />
+                        </div>
+                    ))
+                ) : null}
             </div>
             {!photosCollection || (photosCollection && photosCollection.length === 0 && <p className="text-center text-2xl">No Posts Yet</p>)}
-        </div>
+        </div >
     )
 }
